024a08d6bfacf64171b843b852db8ca1b0fa5824
Munsif Ali <66121828+Munsif-Ali@users.noreply.github.com>
2022-02-23 00:19:47 +0500

    First Commit

---
+++ submit.php
@@ -0,0 +1,11 @@
+<html>
+   <?php?
+    $name=$_GET['name'];
+    $email=$_GET['email'];
+    print "Hello";
+    print $name;
+    print " We will be happy to read your feedback we will contact with you through";
+    print $email;
+   >
+
+</html>
